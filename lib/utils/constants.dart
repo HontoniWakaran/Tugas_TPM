4275d6b1538a9e78603abd067f25bec1f2b3862f
Bintang Jati <bintangid69@gmail.com>
2026-05-05 22:14:50 +0700

    ref

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,7 @@ class Constants {
   // PENTING: Pilih salah satu Base URL di bawah ini sesuai caramu ngetes aplikasi!
 
   // 1. Jika kamu menjalankan aplikasi lewat EMULATOR ANDROID di laptop:
-  static const String baseUrl = 'http://10.0.2.2/catersmart_api';
+  static const String baseUrl = 'http://192.168.1.3/catersmart_api';
 
   // 2. Jika kamu menjalankan aplikasi pakai HP FISIK (dicolok kabel/WiFi):
   // Ganti angka di bawah dengan IPv4 laptopmu (cara ceknya: buka CMD, ketik ipconfig).
